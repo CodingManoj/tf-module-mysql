82fe1d9abb255a092e8c975e3007399b8ee95587
CodingManoj <mvp2612@hotmail.com>
2022-12-23 11:57:38 +0530

    AWS RDS terraform Code which builds mysql with AWS RDS Managed Service

---
+++ data.tf
@@ -0,0 +1,22 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "b50-tf-state-bucket"
+    key    = "vpc/${var.ENV}/terrafom.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+# ref: https://developer.hashicorp.com/terraform/language/state/remote-state-data
+data "aws_secretsmanager_secret" "secrets" {
+  name = "roboshop/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+# output "example" {
+#    value = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
+# }
